633c2f3a1a4a4ca68d0ac10f292c27f4df71bf3c
Julymar Santos Balbi <julymar_santos@ucne.edu.do>
2026-05-29 22:26:19 -0400

    Clase Estudiante con herencia de persona

--- src/Estudiante.java
+++ src/Estudiante.java
@@ -1,3 +1,46 @@
-public class Estudiante {
+public class Estudiante extends Persona {
     
+    private String matricula;
+    private int edad;
+    private String carrera;
+    private String fechaInscripcion;
+
+    public Estudiante (String nombre, String apellido, String matricula, int edad, String carrera, String fechaInscripcion){
+        super(nombre, apellido);
+
+        this.matricula = matricula;
+        this.edad = edad;
+        this.carrera = carrera;
+        this.fechaInscripcion = fechaInscripcion;
+    }
+
+    public String getMatricula(){
+        return matricula;
+    }
+
+    public int getEdad(){
+        return edad;
+    }
+
+    public String getCarrera(){
+        return carrera;
+    }
+
+    public String getFechaInscripcion(){
+        return fechaInscripcion;
+    }
+
+    @Override
+    public void mostrarInfo(){
+        
+        System.out.println("=== Estudiante ===");
+
+        System.out.println("Matricula: " + matricula);
+        System.out.println("Nombre: " + getNombre());
+        System.out.println("Apellido: " + getApellido());
+        System.out.println("Edad: " + edad);
+        System.out.println("Carrera: " + carrera);
+        System.out.println("Fecha de Inscripcion: " + fechaInscripcion);
+
+    }
 }
