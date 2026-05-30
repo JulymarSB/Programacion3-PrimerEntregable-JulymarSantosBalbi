4eedf9eb5658885a824d30b6dcc98e646ddd252a
Julymar Santos Balbi <julymar_santos@ucne.edu.do>
2026-05-29 21:55:36 -0400

    Creada la clase Persona que sera la clase padre de Estudiante y Profesor

--- src/Persona.java
+++ src/Persona.java
@@ -1,3 +1,25 @@
 public class Persona {
-    
+   
+    private String nombre;
+    private String apellido;
+
+    public Persona (String nombre, String apellido){
+
+        this.nombre = nombre;
+        this.apellido = apellido;
+
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public String getApellido(){
+        return apellido;
+    }
+
+    public void mostrarInfo(){
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Apellido: " + apellido);
+    }
 }
