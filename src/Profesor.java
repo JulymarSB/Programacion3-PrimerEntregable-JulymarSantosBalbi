578cae87cc661233ff7e5467eaafb7b5a183a0e2
Julymar Santos Balbi <julymar_santos@ucne.edu.do>
2026-05-29 22:39:27 -0400

    Creada la clase Profesor con herencia de persona

--- src/Profesor.java
+++ src/Profesor.java
@@ -1,3 +1,32 @@
-public class Profesor {
-    
+public class Profesor extends Persona {
+    private String codigo;
+    private String asignaturas;
+
+    public Profesor(String nombre, String apellido, String codigo, String asignaturas){
+        super(nombre, apellido);
+
+        this.codigo = codigo;
+        this.asignaturas = asignaturas;
+
+    }
+
+    public String getCodigo(){
+        return codigo;
+    }
+
+    public String getAsignaturas(){
+        return asignaturas;
+    }
+
+    @Override
+    public void mostrarInfo(){
+
+        System.out.println("=== Profesor ===");
+
+        System.out.println("Codigo: " + codigo);
+        System.out.println("Nombre: " + getNombre());
+        System.out.println("Apellido: " + getApellido());
+        System.out.println("Asignaturas: " + asignaturas);
+
+    }
 }
