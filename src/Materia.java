0a05f83d24c3776dac6944058b72b427069b6c2a
Julymar Santos Balbi <julymar_santos@ucne.edu.do>
2026-05-31 16:20:37 -0400

    Creada la clase materia

--- src/Materia.java
+++ src/Materia.java
@@ -1,3 +1,33 @@
 public class Materia {
     
+    private String codigo;
+    private String nombre;
+    private int creditos;
+
+    public Materia(String codigo, String nombre, int creditos){
+       
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.creditos = creditos;
+    }
+
+    public String getCodigo(){
+        return codigo;
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public int getCreditos(){
+        return creditos;
+    }
+
+    public void mostrarInformacion(){
+        System.out.println("\n=== Materia ===");
+
+        System.out.println("Codigo: " + codigo);
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Creditos: " + creditos);
+    }
 }
